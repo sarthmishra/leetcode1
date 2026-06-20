c3f00aea83b9efc1656aeacccae39882b36896fb
Sarth mishra <154068646+sarthmishra@users.noreply.github.com>
2026-06-20 11:22:00 +0530

    Time: 3 ms (26.68%), Space: 65.2 MB (70.39%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,35 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        /*int[] ans = new int[n];
-        int product = 1;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(i == j){
-                    continue;
-                }
-                product = product * nums[j];
-            }    
-            ans[i] = product;
-            product = 1;
+        int n= nums.length;
+        int[] ans = new int[n];
+        int[] leftproduct = new int[n];
+        int[] rightproduct = new int[n];
+
+        leftproduct[0] = 1;
+        for(int i = 1; i < n; i++){
+            leftproduct[i] = leftproduct[i-1] * nums[i-1];
         }
-        return ans;*/ 
-    int[]prefix = new int[n];
-    int[]suffix = new int[n];
-    int[] ans = new int[n];
-    prefix[0] = 1;
-
-    for(int i = 1; i < n; i++){
-        prefix[i] = prefix[i-1]*nums[i-1];
-    }
-    suffix[n - 1] = 1;
-    for(int i = n-2; i >= 0; i--){
-        suffix[i] = suffix[i+1]*nums[i+1];
-    }
-    for (int i = 0; i < n; i++) {             
-        ans[i] = prefix[i] * suffix[i];         
-        }          
+        rightproduct[n-1] = 1;
+        for(int i = n-2; i > -1; i--){
+            rightproduct[i] = rightproduct[i+1] * nums[i+1];
+        }
+        for(int i = 0; i < n; i++){
+            ans[i] = leftproduct[i] * rightproduct[i];
+        }
         return ans;
-
     }
-}
+}  
