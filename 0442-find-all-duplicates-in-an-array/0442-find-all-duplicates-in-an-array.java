0295571011d32371b9e160172a1455f687ad73c5
Sarth mishra <154068646+sarthmishra@users.noreply.github.com>
2026-07-07 08:50:12 +0530

    Time: 32 ms (10.67%), Space: 72.4 MB (10.31%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        List<Integer>ans = new ArrayList<>();
         int n = nums.length;
-        HashMap<Integer,Integer>map = new LinkedHashMap<>();
-        for(int i = 0; i < n; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        HashMap<Integer,Integer>map = new HashMap<>();
+        List<Integer>ans = new ArrayList<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        for(int key : map.keySet()){
-            if(map.get(key) == 2){
+        for(int key: map.keySet()){
+            if(map.get(key) >= 2){
                 ans.add(key);
             }
         }
-        return ans;
+    return ans;
     }
 }
