6a2de78acb6173ae3d8e8bd6b788e9db7608747f
Sarth mishra <mishrasarth2186@gmail.com>
2026-08-13 13:03:13 +0530

    Time: 31 ms (56.16%), Space: 48.6 MB (50.7%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        int n = candyType.length;
+        HashSet<Integer>set = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            set.add(candyType[i]);
+        }
+        return Math.min(set.size(),n/2);
+    }
+}
