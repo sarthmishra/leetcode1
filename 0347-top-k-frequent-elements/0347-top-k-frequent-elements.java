d691707a6ca79d3efa1c5346fb9c62da31d7c496
Sarth mishra <mishrasarth2186@gmail.com>
2026-08-16 14:53:06 +0530

    Time: 16 ms (37.89%), Space: 47.4 MB (82.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        
         int n = nums.length;
-        HashMap<Integer,Integer>map = new LinkedHashMap<>();
-        for (int i = 0; i < n; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        ArrayList<Integer> keys = new ArrayList<>(map.keySet());
-        // OBJECT (ArrayList): holds all the keys (unique numbers) from the map
+        ArrayList<Integer>keys = new ArrayList<>(map.keySet());
         keys.sort((a,b) -> map.get(b) - map.get(a));
         ArrayList<Integer>list = new ArrayList<>();
-        for (int i = 0; i < k; i++){
+        for(int i = 0; i < k; i++){
             list.add(keys.get(i));
         }
+        
         int[] ans = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
-            ans[i] = list.get(i);
+        for (int i = 0; i < list.size(); i++) {
+            Integer value = list.get(i);
+            ans[i] = (value != null) ? value : 0; 
         }
         return ans;
-
-
-        
     }
 }
