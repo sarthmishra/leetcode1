596a753321dd552831de5bbeaf768e34a118557a
Sarth mishra <mishrasarth2186@gmail.com>
2026-08-21 15:45:42 +0530

    Time: 2 ms (96.8%), Space: 43.7 MB (88.54%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        while(i >= 0|| j >= 0 || carry > 0){
+
+            int digit1 = 0;
+            if(i >= 0){
+                digit1 = num1.charAt(i) - '0';
+            }
+            int digit2 = 0;
+            if(j >= 0){
+                digit2 = num2.charAt(j) - '0';
+            }
+
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 10;
+
+            result.append(sum % 10);
+
+            i--;
+            j--;
+        }
+        return result.reverse().toString();
+    }
+}
